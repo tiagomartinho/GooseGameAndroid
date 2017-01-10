7306f3fd8209032555ab39c73db316adaaeddfb9
tiagomartinho <tiago.martinho@xpeppers.com>
2017-01-10 14:11:24 +0100

    extract game construction to setup

--- app/src/test/java/com/xp/goosegame/GameTest.java
+++ app/src/test/java/com/xp/goosegame/GameTest.java
@@ -1,5 +1,6 @@
 package com.xp.goosegame;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -9,10 +10,16 @@ import static junit.framework.Assert.assertEquals;
 @RunWith(MockitoJUnitRunner.class)
 public class GameTest {
 
+    private Game game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+    }
+
     @Test
     public void
     when_the_game_starts_it_has_no_players() throws Exception {
-        Game game = new Game();
 
         assertEquals(0, game.numberOfPlayers());
     }
@@ -20,7 +27,6 @@ public class GameTest {
     @Test
     public void
     when_adding_the_first_player_the_number_of_players_increases() throws Exception {
-        Game game = new Game();
 
         game.add(new Player("Pippo"));
 
