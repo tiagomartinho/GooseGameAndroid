adcb63080c51b682c76a981197762cdc2cadf79d
tiagomartinho <tiago.martinho@xpeppers.com>
2017-01-10 14:44:13 +0100

    extracts InputParser to separate file

---
+++ app/src/main/java/com/xp/goosegame/InputParser.java
@@ -0,0 +1,17 @@
+package com.xp.goosegame;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class InputParser {
+
+    public Player parse(String input) {
+        Pattern p = Pattern.compile("aggiungi giocatore (\\w+)");
+        Matcher m = p.matcher(input);
+        if (m.find()) {
+           return new Player(m.group(1));
+        } else {
+            return null;
+        }
+    }
+}
--- app/src/test/java/com/xp/goosegame/ParserTest.java
+++ app/src/test/java/com/xp/goosegame/ParserTest.java
@@ -4,12 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static junit.framework.Assert.assertEquals;
-
-
 
 @RunWith(MockitoJUnitRunner.class)
 public class ParserTest {
@@ -19,23 +14,11 @@ public class ParserTest {
     add_player_command() throws Exception {
         String input = "aggiungi giocatore Pippo";
         Player expectedPlayer = new Player("Pippo");
-
         InputParser parser = new InputParser();
+
         Player player = parser.parse(input);
 
         assertEquals(expectedPlayer, player);
     }
 
-    private class InputParser {
-
-        public Player parse(String input) {
-            Pattern p = Pattern.compile("aggiungi giocatore (\\w+)");
-            Matcher m = p.matcher(input);
-            if (m.find()) {
-               return new Player(m.group(1));
-            } else {
-                return null;
-            }
-        }
-    }
 }
